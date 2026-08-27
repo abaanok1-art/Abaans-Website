e3e6c511133ca8879d13fd5da7f5cc3cb693e61d
AbaanArt <abaanok1@gmail.com>
2026-08-27 01:52:46 -0700

    Ready to deploy

--- src/Components/HomePage/home.jsx
+++ src/Components/HomePage/home.jsx
@@ -14,7 +14,7 @@ const Homepage = () => {
         <div id="home" className="Homemain">
         <h1 className="Title">Welcome To <br />Abaans Website!</h1>
         <p className="intro">
-            I&apos;m Abaan Mubeen, a student and full-stack web developer. I build modern, responsive websites with React, JavaScript, and Node.js.
+            Im Abaan Mubeen, a student and full-stack web developer. I build modern, responsive websites with React, JavaScript, and Node.js.
         </p>
         <img className="spinner" src={SPINER} alt="React logo" width={100} />
         </div>
